be428a07681ceb921cbaac6558a0d14c9efbb2ad
Berlin <tommyonik@gmail.com>
2024-01-14 23:39:53 -0500

    add index route to display all flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,20 @@
 import { Flight } from "../models/flight.js"
 
+
+function index(req, res){
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: "All Flights"
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+    })
+}
+
 function newFlight(req, res){
   res.render("flights/new", {
     title: "Add Flight"
@@ -7,17 +22,20 @@ function newFlight(req, res){
 }
 
 function create(req,res){
-Flight.create(req.body)
-.then(flight => {
-  res.redirect('/flights/new')
-})
-.catch(error => {
-  console.log(error)
-  res.redirect('/flights/new')
-})
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights/new')
+  })
 }
 
+
+
 export {
+  index,
   newFlight as new,
   create,
 }
